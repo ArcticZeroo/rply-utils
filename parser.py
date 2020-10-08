0cbecc4532f9034336a94bc0976b5458a05c9e3e
Spencer <spencer@frozor.io>
2020-10-07 23:34:52 -0400

    Minor parser fixes to appease rply - Use .error instead of .error_handler (the latter is the property that .error sets) - return the old method when wrapping it so that we can have multiple production rules with the same decorator - return a noop for empty productions in case someone tries to use it as a decorator - use relative importing

--- parser.py
+++ parser.py
@@ -1,9 +1,10 @@
 from typing import Union, List, Tuple, Callable, Optional, Iterable
 
-from exceptions import ParsingTokensExhaustedException, ParsingException, IllegalArgumentException, LexingException
-from lexer import Lexer
 from rply import Token, ParserGenerator
-from util import is_lexing_error
+
+from .exceptions import ParsingTokensExhaustedException, ParsingException, IllegalArgumentException, LexingException
+from .lexer import Lexer
+from .util import is_lexing_error
 
 LeftOrRight = Union['left', 'right']
 ParserPrecedence = List[Tuple[LeftOrRight, List[str]]]
@@ -23,9 +24,9 @@ class Parser:
     def __init__(self, lexer_or_tokens: Union[Lexer, Iterable[str]], precedence: Optional[ParserPrecedence] = None):
         self._pg = ParserGenerator(
             lexer_or_tokens.possible_tokens if isinstance(lexer_or_tokens, Lexer) else lexer_or_tokens,
-            precedence
+            precedence or []
         )
-        self._pg.error_handler(self._handle_error)
+        self._pg.error(self._handle_error)
 
     def _handle_error(self, token: Token):
         if isinstance(token, Token) and token.name == END_TOKEN_NAME:
@@ -35,19 +36,24 @@ class Parser:
         # if user error handler doesn't raise, rply is going to raise a really unhelpful exception instead.
         raise ParsingException(token)
 
-    def error_handler(self, method):
+    def error(self, method):
         self._user_error_handler = method
 
     def production(self, rule: str):
         if PRODUCTION_NAME_SEPARATOR not in rule:
             raise IllegalArgumentException(f'Rule must contain "{rule}"')
 
-        return lambda method: self._pg.production(rule)(method)
+        def wrap_method(method):
+            self._pg.production(rule)(method)
+            return method
+
+        return wrap_method
 
     def empty_production(self, name_or_rule: str):
         if PRODUCTION_NAME_SEPARATOR not in name_or_rule:
             name_or_rule = name_or_rule.strip() + PRODUCTION_NAME_SEPARATOR
         self._pg.production(name_or_rule)(_noop)
+        return _noop
 
     def build(self) -> 'Parser':
         """
