a72cf7aadc6962a9c44e6c055a87c7fcf44636fa
Spencer <spencer@frozor.io>
2020-10-07 23:38:41 -0400

    Add identity tokens to lexer

--- lexer.py
+++ lexer.py
@@ -12,16 +12,23 @@ class Lexer:
         self._lg = LexerGenerator()
         self._possible_tokens = set()
 
+    @staticmethod
+    def _enum_name_or_str(enum_or_str: Union[Enum, str]) -> str:
+        return enum_or_str.name if isinstance(enum_or_str, Enum) else enum_or_str
+
     @property
     def possible_tokens(self) -> Iterable[str]:
         return self._possible_tokens.copy()
 
     def add(self, token_name: Union[Enum, str], pattern: str, regex_flags: int = 0) -> 'Lexer':
-        if isinstance(token_name, Enum):
-            token_name = token_name.name
+        token_name = self._enum_name_or_str(token_name)
         self._possible_tokens.add(token_name)
         self._lg.add(token_name, pattern, regex_flags)
         return self
+
+    def add_identity(self, token_name: Union[Enum, str]):
+        token_name = self._enum_name_or_str(token_name)
+        return self.add(token_name, token_name)
 
     def ignore(self, pattern: str, regex_flags: int = 0) -> 'Lexer':
         self._lg.ignore(pattern, regex_flags)
